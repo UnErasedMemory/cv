f1b9b83d9172889cd466e94dfcddd489150d273f
UnErasedMemory <petraxandika@gmail.com>
2025-06-11 16:33:33 +0700

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-function showSection(id) {
-  const sections = document.querySelectorAll("main section");
-  sections.forEach((section) => section.classList.remove("active"));
-  document.getElementById(id).classList.add("active");
-}
